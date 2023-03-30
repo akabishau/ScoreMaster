993076b1fec0be7a87d26f08105ea3eb5cb55457
akabishau <aleksey.kabishau@icloud.com>
2023-03-29 22:07:12 -0400

    Initial Commit

---
+++ ScoreMaster/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoreMaster
+//
+//  Created by Aleksey Kabishau on 3/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		view.backgroundColor = .systemRed
+	}
+
+
+}
+
